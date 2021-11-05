35d5bf9e14dca95af81f674faf669d4ec2ea2b16
Your Name <you@example.com>
2021-11-05 15:02:58 +0800

    change

--- components/answericon.js
+++ components/answericon.js
@@ -1,40 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { AiOutlineBook } from "react-icons/ai";
-import Button from "react-bootstrap/Button";
-import ModalHeader from "react-bootstrap/ModalHeader";
-import ModalFooter from "react-bootstrap/ModalFooter";
-import ModalBody from "react-bootstrap/ModalBody";
-import Modal from "react-bootstrap/Modal";
+
 const AnswerIcon = () => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+  const [showModal, setShowModal] = React.useState(false);
   return (
     <>
-      <Button
-        className=" bg-blue-500 text-white active:bg-blue-600 mx-20 my-4	justify-center	 p-8 rounded shadow hover:shadow-md outline-none focus:outline-none ease-linear transition-all duration-150"
+      <button
+        className=" my-4	justify-center bg-blue-500 text-white active:bg-blue-600 mx-20 p-8 rounded shadow hover:shadow-md outline-none focus:outline-none ease-linear transition-all duration-150"
         type="button"
-        onClick={handleShow}
+        onClick={() => setShowModal(true)}
       >
         <AiOutlineBook />
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <ModalHeader closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </ModalHeader>
-        <ModalBody> Answersheet will show up</ModalBody>
-        <ModalFooter>
-          <Button onClick={handleClose} variant="outline-primary">
-            Close
-          </Button>
-          <Button onClick={handleClose} variant="outline-primary">
-            Save Changes
-          </Button>
-        </ModalFooter>
-      </Modal>
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                    I always felt like I could do anything. That’s the main
+                    thing people are controlled by! Thoughts- their perception
+                    of themselves! They're slowed down by their perception of
+                    themselves. If you're taught you can’t do anything, you
+                    won’t do anything. I was taught I could do everything.
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-black active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </>
   );
 };
