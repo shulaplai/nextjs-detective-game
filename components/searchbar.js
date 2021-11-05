ef104c93b8905ad0ec17a17cf9f679a1b5ca7477
Your Name <you@example.com>
2021-11-05 10:32:06 +0800

    change

---
+++ components/searchbar.js
@@ -0,0 +1,20 @@
+const SearchBar = () => {
+  return (
+    <div className=" pt-56 p-20	">
+      <div className="bg-white flex items-center justify-self-center rounded-full shadow-xl">
+        <input
+          className="rounded-l-full w-full  px-6 text-gray-700 leading-tight focus:outline-none"
+          id="search"
+          type="text"
+          placeholder="Search"
+        />
+
+        <div className="p-4">
+          <button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
+            icon
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,8 @@ export default function Home({}) {
   return (
     <div className=" pt-56 p-20	">
       <Head></Head>
-      <div className="bg-white flex items-center justify-self-center rounded-full shadow-xl">
-        <input
-          className="rounded-l-full w-full  px-6 text-gray-700 leading-tight focus:outline-none"
-          id="search"
-          type="text"
-          placeholder="Search"
-        />
-
-        <div className="p-4">
-          <button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
-            icon
-          </button>
-        </div>
-        <button
-          class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-          type="button"
-        >
-          <i class="fas fa-heart"></i>
-        </button>
-      </div>
+     
+    
     </div>
   );
 }
