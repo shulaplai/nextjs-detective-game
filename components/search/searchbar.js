3c4db44a56d79185208f19e60aa0a249a5250fa6
Your Name <you@example.com>
2021-11-12 08:28:19 +0800

    update

--- components/search/searchbar.js
+++ components/search/searchbar.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-// import { getMongodbQuestion } from "../pages/api/mongodb.js";
+import { getMongodbQuestion } from "../../pages/api/mongodb.js";
 // import question from "../question.js";
 
 const SearchBar = () => {
+   const [question, setquestion] = useState();
+ const onChangeHandler = (event) => {
+   setquestion(event.target.value);}
   return (
     <div className=" pt-40 p-20	">
       <div className="bg-white flex items-center justify-self-center rounded-full shadow-xl">
@@ -11,13 +14,18 @@ const SearchBar = () => {
           id="search"
           type="text"
           placeholder="Search"
+          value={question}
+          onChange={onChangeHandler}
         />
 
         <div className="p-4">
-          <button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
+          <button
+            className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"
+            onClick={() => {
+              question === "sam" ? getMongodbQuestion : null;
+            }}
+          >
             icon
-            //onclick get mongodb
-            get explain based on question
           </button>
         </div>
       </div>
--- pages/api/mongodb.js
+++ pages/api/mongodb.js
@@ -1,4 +1,3 @@
-
 import { MongoClient } from "mongodb";
 
 // Connection URL
@@ -14,12 +13,12 @@ export async function getMongodbQuestion(question) {
   await client.connect();
   console.log("Connected successfully to server");
   const db = client.db(dbName);
-  const collection = db.collection("documents");
+  const collection = db.collection("question");
 
   // the following code examples can be pasted here...
-
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+  const filteredDocs = await collection.find({ a: 3 }).toArray();
+  console.log("Found documents filtered by { a: 3 } =>", filteredDocs);
   return "done.";
 }
-
-const filteredDocs = await collection.find({ a: 3 }).toArray();
-console.log("Found documents filtered by { a: 3 } =>", filteredDocs);
