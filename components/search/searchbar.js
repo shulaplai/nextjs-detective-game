470e9b4a2ff9e135cea238355f1c6a652fb6eca4
Your Name <you@example.com>
2021-11-23 11:38:36 +0800

    Update searchbar.js

--- components/search/searchbar.js
+++ components/search/searchbar.js
@@ -40,7 +40,7 @@ const SearchBar = () => {
         <Autocomplete
           id="highlights-demo"
           isOptionEqualToValue={(option, value) => option.id === value.id}
-          className="rounded-l-full w-screen	  text-gray-700  focus:outline-none"
+          className="rounded-l-full px-40	  text-gray-700  focus:outline-none"
           sx={{ width: 300 }}
           inputValue={inputValue}
           onInputChange={(event, newInputValue) => {
