ac1e93db201786283d5a6ea97c991adc2af14ce3
shulaplai <75302349+shulaplai@users.noreply.github.com>
2021-12-06 11:40:37 +0800

    Update helpicon.js

--- components/helpicon.js
+++ components/helpicon.js
@@ -36,11 +36,7 @@ const HelpIcon = () => {
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                    希臘棺材I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                    希臘棺材like case
                   </p>
                 </div>
                 {/*footer*/}
