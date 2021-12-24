ae81cfef9e37b2b33aa93bcabf8d894bfba67320
shulaplai <75302349+shulaplai@users.noreply.github.com>
2021-12-24 18:28:59 +0800

    Update data.js

--- components/data.js
+++ components/data.js
@@ -46,47 +46,127 @@ export const QuestionData = [
   },
   {
     request: "私家偵探協會",
-    answer: `aaaaaaaaaaaaaaa{sss
-    s    }aaaaaaaaaaaaa`,
+    answer: `這個協會是由於警隊改革而誕生的機構
+由於市民對貪污腐敗嘅警察強烈不滿
+市長於20年前進行改革將警力大幅下降
+本來的權力被分散一部分交由其他部門
+有一部分落於民間
+私家偵探協會便是提供警隊例如保安巡查調查等服務
+
+在前會長就任時
+15年前至十年前
+二代會長
+他鼓吹擴張協會擴張至全國
+但由於偵探質素參差所以當時社會對於偵探的印象十分差 認為他們是 打手多過偵探
+似黑社會
+他在十年前由於懷疑貪污引咎辭職
+但由於缺乏證據
+證人失蹤所以最後並沒有被告上法庭
+
+現在的私家偵探協會是一個人數多於一萬還誇全國嘅強大私人機構
+但是近年開始被政府打壓部分人員也被或挖角至各政府部門
+`,
   },
   {
     request: "律師",
-    answer: `dddddddddddd`,
+    answer: `遵守和客戶的約定
+因為鐵盒不小,不想隨身攜帶
+所以放喺禮堂
+打算之後先開啟
+開啓鐵盒的鑰匙一直在他的口袋中
+
+`,
   },
   {
     request: "管理人資料",
-    answer: `"john smith"`,
+    answer: `林介詞
+前任私家偵探
+當知道遺囑失蹤後立刻報告
+並阻止所有人離開現場
+之後和到來的偵探一起進行搜身檢查
+`,
   },
   {
     request: "工人林禪資料",
-    answer: `"john smith"`,
+    answer: `廚師
+因為偷竊所以曾被前會長警告要炒佢
+`,
   },
   {
     request: "工人臨安娜資料",
-    answer: `"john smith"`,
+    answer: `受前會長喜愛
+知道前會長死前一天有秘密會談
+聽從前會長指示
+開後門神秘的客人入嚟
+前會長叫他忘記會談
+並叫他唔好同任何人講
+`,
   },
   {
     request: "親人一號妻子6資料",
-    answer: `"john smith"`,
+    answer: `年輕貌美
+背後對前會長進行下毒
+所以懼怕驗屍
+偷走屍體
+
+`,
   },
   {
     request: "親人2號女兒馬六資料",
-    answer: `"john smith"`,
+    answer: `親人2號女兒馬六馬偲嫣
+和前會長有金錢衝突`,
   },
   {
     request: "親人3號大仔馬四資料",
-    answer: `"john smith"`,
+    answer: `親人3號大仔馬四馬思純
+和前會長一向關係差`,
   },
   {
     request: "親屬4號小兒子馬五資料",
-    answer: `"john smith"`,
+    answer: `馬師道
+一向和前會長關係好但最近有衝突
+以為遺囑上對他不利所以偷走並將遺囑放入棺材中
+`,
   },
   {
     request: "其後發展",
-    answer: `"john smith"`,
+    answer: `千鳥到場進行搜身檢查一無所穫
+思索一晚過後
+認為遺囑是被收藏到
+但打開棺材發現入面的屍體不見了
+也沒有失蹤的遺囑
+只有一堆債券
+`,
   },
   {
     request: "其後發展2",
-    answer: `"john smith"`,
+    answer: `管理人和前會長以前曾執行過秘密任務
+管理人代表著某黑幫
+在秘密會談
+和前會長發生衝突
+then peace 
+but he died suddenly 
+the secret is in the box
+secret is that they use the pda to disappeared people and body , this graveyard is full of illegal body in other area, where ppl don’t go 
+
+little boy thought the wish will cut him out 
+so stole it and hide it in the coffin
+
+then manager want to find the wish , have the graveyard surrounded
+but lawyer said the wish is gone 
+manager shocked, called his boss. 
+then call pda, try to find it 
+
+his boss (me)find the wish in the coffin 
+took it , put the money that they promised to give down
+
+the wife and sister  , fucked together 
+so try to poison the father 
+ thought they kill him
+but the poison is fake 
+they stole the body during the night , they sleep together 
+, they are lady , no other watching 
+hide it in other people ‘s grave 
+`,
   },
 ];
